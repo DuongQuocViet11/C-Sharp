39de285b4a84e9d6a8d8f492057af00ed321c786
duongquocv <duongquocv@gmail.com>
2021-06-29 22:14:03 +0700

    Task04

---
+++ Task04/Employee1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Anonymous_Type
+{
+    class Employee
+    {
+        public void DisplayDetails(object emp)
+        {
+            string fName = "";
+            string lName = "";
+            int age = 0;
+            PropertyInfo[] attrs = emp.GetType().GetProperties();
+            foreach (PropertyInfo attr in attrs)
+            {
+                switch (attr.Name)
+                {
+                    case "FirstName":
+                        fName = attr.GetValue(emp, null).ToString();
+                        break;
+                    case "LastName":
+                        lName = attr.GetValue(emp, null).ToString();
+                        break;
+                    case "Age":
+                        age = (int)attr.GetValue(emp, null);
+                        break;
+                }
+            }
+            Console.WriteLine("Name: {0}  {1}, Age: {2}", fName, lName, age);
+        }
+    }
+}
---
+++ Task04/Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stock = new { Name = "Michgan Enterpries", Code = 1301, Price = 35056.75 };
+            Console.WriteLine("Stock Name: " + stock.Name);
+            Console.WriteLine("Stock Code: " + stock.Code);
+            Console.WriteLine("Stock Price: " + stock.Price);
+        }
+
+        public static void Main(string[] args)
+        {
+            Employee david = new Employee();
+            david.DisplayDetails(new { FirstName = "David", LastName = "Olake", Age = 30 });
+            Console.Read();
+        }
+    }
+}
