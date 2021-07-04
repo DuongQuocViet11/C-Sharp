9cc253eac517a1d6e7d686a0907c2f0f1014e7ba
duongquocv <duongquocv@gmail.com>
2021-07-04 17:49:06 +0700

    Task07

---
+++ Task07/Task07/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Animal
+{
+    interface Animal
+    {
+        void Habitat();
+    }
+    class Dog : Animal
+    {
+        public void Habitat()
+        {
+            Console.WriteLine("Can be housed with human beings");
+        }
+        static void Main(string[] args)
+        {
+            Dog objDog = new Dog();
+            Console.WriteLine(objDog.GetType().Name);
+            objDog.Habitat();
+        }
+    }
+}
