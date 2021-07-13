0f0d6f1623a2057faeb8b2c1ff56d4bda666be6f
duongquocv <duongquocv@gmail.com>
2021-07-13 23:12:19 +0700

    Task08

---
+++ Task08/Mod2_Lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mod2_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee1 = new Employee("libby", 2000);
+            var employee2 = new TechnicalEmployee("Zaynah");
+            var employee3 = new BusinessEmployee("Winter");
+
+            Console.WriteLine(employee1.employeeStatus() + "..." + employee2.employeeStatus() + "..." + employee3.employeeStatus());
+        }
+    }
+}
